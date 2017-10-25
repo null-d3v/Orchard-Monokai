4d9cedaccf8bfb931e41cd67df638c75c9355717
Benjamin Soddy <benjamin.soddy@gmail.com>
2017-10-24 19:30:41 -0500

    fix: Scroll header detection with Chrome update

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -4,21 +4,24 @@
 
     var headerHeight = header.offsetHeight;
     var headerY = headerHeight;
-    var scrollY = document.body.scrollTop;
+    var scrollY = document.body.scrollTop ||
+        document.documentElement.scrollTop;
 
     window.addEventListener("scroll", function()
+    {
+        var scrollTop = document.body.scrollTop ||
+            document.documentElement.scrollTop;
+        headerY += scrollY - scrollTop;
+        if (headerY > headerHeight)
         {
-            headerY += scrollY - document.body.scrollTop;
-            if (headerY > headerHeight)
-            {
-                headerY = headerHeight;
-            }
-            else if (headerY < 0)
-            {
-                headerY = 0;
-            }
-            header.style.transform =
-                "translateY(" + (headerY - headerHeight) + "px)";
-            scrollY = document.body.scrollTop;
-        });
+            headerY = headerHeight;
+        }
+        else if (headerY < 0)
+        {
+            headerY = 0;
+        }
+        header.style.transform =
+            "translateY(" + (headerY - headerHeight) + "px)";
+        scrollY = scrollTop;
+    });
 })();
